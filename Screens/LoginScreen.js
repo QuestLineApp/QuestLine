40aaed0047fa0dd18c99bb28afbc82b736b9c42f
Ammon <ammonhepworth@outlook.com>
2018-03-01 12:30:36 -0700

    add button for logging in with google

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { Icon, Button, Container, Header, Content, Left } from 'native-base';
+import Expo from 'expo'
 
 import HomeScreen from './HomeScreen';
 
 class LoginScreen extends Component {
+  signInWithGoogleAsync = async () => {
+    try {
+      const result = await Expo.Google.logInAsync({
+				behavior: 'standalone',
+        androidClientId: ' 891799707034-trepglhtmhjfoms1v7ga8nasj543fcn2.apps.googleusercontent.com ',
+        iosClientId: process.env.GOOGLE_IOS_CLIENT_ID,
+        scopes: ['profile'],
+      })
+
+      if (result.type === 'success') {
+        return result
+      }
+      return { cancelled: true }
+    } catch (e) {
+      return { error: e }
+    }
+  }
+
+
+  onLoginPress = async () => {
+    const result = await this.signInWithGoogleAsync()
+    // if there is no result.error or result.cancelled, the user is logged in
+    // do something with the result
+  }
+
   render() {
     return (
       <Container>
@@ -17,8 +43,9 @@ class LoginScreen extends Component {
           <Text>Welcome to Questline</Text>
           <Text> Please Login </Text>
           <Button block light onPress={() => this.props.navigation.navigate('HomeScreen')}>
-            <Text> Login </Text>
+            <Text> Google Login </Text>
           </Button>
+          <Button onPress={this.onLoginPress}><Text>Login</Text></Button>
         </Content>
 
       </Container>
