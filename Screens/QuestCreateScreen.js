de840b80b065e06b28636a67f8e684b415fc8d11
Ammon <ammonhepworth@outlook.com>
2018-03-27 23:23:44 -0600

    got quest saving to work

--- Screens/QuestCreateScreen.js
+++ Screens/QuestCreateScreen.js
@@ -1,12 +1,47 @@
 //https://www.flaticon.com/free-icon/settings_263074
 import React, { Component } from 'react';
-import { Image, View, Text, StyleSheet, TextInput, Picker} from 'react-native';
+import { AsyncStorage, Image, View, Text, StyleSheet, TextInput, Picker} from 'react-native';
 
 import { Icon, Button, Container, Header, Content, Left } from 'native-base';
 
 import PickerExample from './CatPicker.js'
 
 class QuestCreateScreen extends Component {
+
+	componentDidMount() {
+		AsyncStorage.getItem('localQuestList').then( value => {
+			this.setState({'localQuestList': JSON.parse(value) });
+			console.log(this.state.localQuestList);
+		});
+	}
+
+	saveQuest() {
+		let newQuest = {
+			'name' : this.state.questName,
+			'description' : this.state.description,
+		}
+		console.log(newQuest);
+		let questList = this.state.localQuestList;
+		console.log(questList);
+		if(questList === null) questList = { list : [] };
+		questList.list.push(newQuest);
+		console.log('attempt set ');
+		console.log(questList);
+		
+		AsyncStorage.setItem('localQuestList',JSON.stringify(questList));
+		console.log('set ' + questList);
+
+	}
+
+	componentWillUnmount() {
+		console.log('unmounting quest creation');
+	}
+
+	showState() {
+		console.log(this.state);
+	}
+
+
   static navigationOptions = {
     drawerIcon: (
       <Image source={require('../assets/settingsIcon.png')}
@@ -30,11 +65,16 @@ class QuestCreateScreen extends Component {
         	
 					<Text> New Quest Screen</Text>
 					
-					<TextInput style={{height: 40}} placeholder="Quest Name" onChangeText={(text) => this.setState({text})} />
+					<TextInput style={{height: 40}} placeholder="Quest Name" onChangeText={(text) => this.setState({'questName' : text})} />
 					
-					<TextInput style={{height: 40}} placeholder="Description" onChangeText={(text) => this.setState({text})} />
+					<TextInput style={{height: 40}} placeholder="Description" onChangeText={(text) => this.setState({'description' : text})} />
+
 					
-					<PickerExample />
+
+					<Button block light onPress={()=> this.showState()}><Text> Log state </Text></Button>
+
+					<Button block light onPress={()=> this.saveQuest()}><Text> Save Quest </Text></Button>
+
         </Content>
 
       </Container>
--- Screens/SettingsScreen.js
+++ Screens/SettingsScreen.js
@@ -1,10 +1,34 @@
 //https://www.flaticon.com/free-icon/settings_263074
 import React, { Component } from 'react';
-import { Image, View, Text, StyleSheet } from 'react-native';
+import { AsyncStorage, Image, View, Text, StyleSheet } from 'react-native';
 
 import { Icon, Button, Container, Header, Content, Left } from 'native-base';
 
 class SettingsScreen extends Component {
+	state = {
+		'numclicks' : 0
+	}
+
+	componentDidMount() { 
+		
+		AsyncStorage.getItem('numclicks').then( value => {
+			this.setState({'numclicks': parseInt(value) });
+			console.log('pulled ' + value);
+		});
+		console.log(this.state.numclicks);
+	}
+
+	incrementClicks() {
+		this.state.numclicks += 1;
+		this.forceUpdate();
+		console.log(this.state.numclicks);
+	}
+
+	componentWillUnmount() {
+		AsyncStorage.setItem('numclicks', this.state.numclicks.toString());
+		console.log('set ' + this.state.numclicks);
+	}
+
   static navigationOptions = {
     drawerIcon: (
       <Image source={require('../assets/settingsIcon.png')}
@@ -28,6 +52,7 @@ class SettingsScreen extends Component {
           justifyContent : 'center'
         }}>
         <Text> Settings Screen </Text>
+      <Button block light onPress={()=> this.incrementClicks()}><Text>{this.state.numclicks}</Text></Button>
         </Content>
       </Container>
     );
