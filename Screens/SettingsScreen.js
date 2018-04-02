fb45ff26e31b7a395c1e874ed172ed690ffaad99
Ammon <ammonhepworth@outlook.com>
2018-04-02 15:09:37 -0600

    remove asyncstorage from settings test

--- Screens/SettingsScreen.js
+++ Screens/SettingsScreen.js
@@ -5,29 +5,6 @@ import { AsyncStorage, Image, View, Text, StyleSheet } from 'react-native';
 import { Icon, Button, Container, Header, Content, Left } from 'native-base';
 
 class SettingsScreen extends Component {
-	state = {
-		'numclicks' : 0
-	}
-
-	componentDidMount() { 
-		
-		AsyncStorage.getItem('numclicks').then( value => {
-			this.setState({'numclicks': parseInt(value) });
-			console.log('pulled ' + value);
-		});
-		console.log(this.state.numclicks);
-	}
-
-	incrementClicks() {
-		this.state.numclicks += 1;
-		this.forceUpdate();
-		console.log(this.state.numclicks);
-	}
-
-	componentWillUnmount() {
-		AsyncStorage.setItem('numclicks', this.state.numclicks.toString());
-		console.log('set ' + this.state.numclicks);
-	}
 
   static navigationOptions = {
     drawerIcon: (
@@ -52,7 +29,6 @@ class SettingsScreen extends Component {
           justifyContent : 'center'
         }}>
         <Text> Settings Screen </Text>
-      <Button block light onPress={()=> this.incrementClicks()}><Text>{this.state.numclicks}</Text></Button>
         </Content>
       </Container>
     );
