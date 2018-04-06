8d9fa47052e7a249c288d0af5e7bc26d60c0ddaf
Ammon Hepworth <ammonhepworth@outlook.com>
2018-04-06 00:21:58 -0600

    Quest list (#13)
    * add quest list screen with delete and edit buttons
    
    * ree
    
    * add functionality to delete quests
    
    * remove tabs and trailing white space

--- Screens/QuestListScreen.js
+++ Screens/QuestListScreen.js
@@ -9,85 +9,113 @@ import HomeScreen from './HomeScreen';
 
 class QuestListScreen extends Component {
 
-	componentDidMount() {
-		AsyncStorage.getItem('localQuestList').then( value => {
-			this.setState({'localQuestList': JSON.parse(value) });
-			console.log(this.state.localQuestList);
-		});
-	}
+  ree = false;
 
-  deleteQuest() {
+  constructor(props){
+    super(props);
+    AsyncStorage.getItem('localQuestList').then( value => {
+      this.setState({'localQuestList': JSON.parse(value) });
+      console.log('mountin and set: ' + this.state.localQuestList);
+      this.ree = true;
+      this.forceUpdate();
+    });
+    console.log('construct');
   }
+
+  componentWillMount() {
+    this.setState({'localQuestList': {'list': []}});
+  }
+
+  saveQuestList() {
+    AsyncStorage.setItem('localQuestList',JSON.stringify(this.state.localQuestList));
+  }
+  deleteQuest(idx) {
+    this.state.localQuestList.list.splice(idx,1);
+    this.saveQuestList();
+    this.forceUpdate();
+  }
+
 
   editQuest() {
 
   }
 
-
-  render() {
-    let ree = {localQuests: [{name:'dingle',desc:'yo this be dingle'},{name:'dongle',desc:'but hey this is a dongle and I really like dongles so this text field is going to be really long so I can espouse just how much I love my dongles, da dingle dongol mingle mongol. Dang, Ive never even though about the mongols making apps. I wonder if they would have an app that would generate dope throat singing shit or if they would have a horse companion in a war game that they are met on an open field lol.'}]};
-
-    let quests = ree.localQuests.map(( item, key ) =>
-    {
-      return(
-          <View style = {{height:100,width:'90%',backgroundColor:'#26A69A',alignItems:'center',alignSelf:'center',margin: 4}}>
-            <Text style= {{'fontWeight': 'bold'}}> {item.name}</Text>
-            <Text numberOfLines={2}> {item.desc}</Text>
-            <View style={{
-              height:20,
+  getQuests() {
+    let quests = this.state.localQuestList.list.map(( item, idx ) =>
+      {
+        return(
+          <View key={idx} style = {{height:100,width:'90%',backgroundColor:'#26A69A',alignItems:'center',alignSelf:'center',margin: 4}}>
+          <Text style= {{'fontWeight': 'bold'}}> {item.name}</Text>
+          <Text numberOfLines={2}> {item.description}</Text>
+          <View style={{
+            height:20,
               position:'absolute',
               bottom:10,
-              flex:1, 
-              flexDirection: 'row', 
-              width:'80%', 
-            }}>
-            <View style={{flex:1}}>
-              <Button block style = {{
-                height:20,
-                position:'absolute',
-                bottom:0,
-                backgroundColor:'#ffd150',
-                width:'90%', 
-                alignSelf:'center'}} 
-                onPress={()=> this.editQuest(item.desc)}>
-                <Text> Edit </Text>
-              </Button>
-            </View>
-            <View style={{flex:1}}>
-              <Button block style = {{
-                height:20,
-                position:'absolute',
-                bottom:0,
-                backgroundColor:'#cc5050',
-                width:'90%', 
-                alignSelf:'center',
-                }} onPress={()=> this.deleteQuest(item.desc)}>
-                <Text> Delete </Text>
-              </Button>
-            </View>
-            </View>
+              flex:1,
+              flexDirection: 'row',
+              width:'80%',
+          }}>
+          <View style={{flex:1}}>
+          <Button block style = {{
+            height:20,
+              position:'absolute',
+              bottom:0,
+              backgroundColor:'#ffd150',
+              width:'90%',
+              alignSelf:'center'}}
+          onPress={()=> this.editQuest(idx)}>
+          <Text> Edit </Text>
+          </Button>
           </View>
-      );
-    });
+          <View style={{flex:1}}>
+          <Button block style = {{
+            height:20,
+              position:'absolute',
+              bottom:0,
+              backgroundColor:'#cc5050',
+              width:'90%',
+              alignSelf:'center',
+          }} onPress={()=> this.deleteQuest(idx)}>
+          <Text> Delete </Text>
+          </Button>
+          </View>
+          </View>
+          </View>
+        );
+      });
+    return quests;
+  }
 
-    
+  render() {
+    if(this.ree === false) {
+      return(<Text>Loading...</Text>);
+    }
+    console.log(this.state.localQuestList);
     return (
+      <Container>
+      <Header style={{backgroundColor:'#246A73'}}>
+      <Left>
+      <Button transparent>
+      <Icon name='menu' onPress={()=>this.props.navigation.navigate('DrawerOpen')} />
+      </Button>
+      </Left>
+      </Header>
       <Content contentContainerStyle= {{
-          flex: 1,
+        flex: 1,
           alignItems: 'center',
           justifyContent : 'center',
           width: '100%',
       }}>
-        <Text style = {{'fontWeight':'bold', 'fontSize':20}}>Quest Log</Text>
-        <ScrollView style= {{width:'90%'}}>
-          {
-            quests
-          }
+      <Text style = {{'fontWeight':'bold', 'fontSize':20}}>Quest Log</Text>
+      <ScrollView style= {{width:'90%'}}>
+      {
+        this.getQuests()
+      }
 
-        </ScrollView>
+      </ScrollView>
 
       </Content>
-
+      </Container>
     );
   }
 }
