c88b0a34679227a73b72624630d383958161b44b
Philip Nelson <philipnelson5@gmail.com>
2018-04-18 20:31:12 -0600

    Put quests on map (#20)
    * icons on maps now
    
    * added detail when click popup bubble

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,6 +1,6 @@
 //https://www.flaticon.com/free-icon/home_263115#term=home&page=1&position=1
 import React, { Component } from 'react';
-import { Image, View, Text, StyleSheet } from 'react-native';
+import { Alert, Image, AsyncStorage, View, Text, StyleSheet } from 'react-native';
 
 import { TabNavigator } from 'react-navigation';
 import { DrawerNavigator, DrawerItems } from 'react-navigation';
@@ -13,114 +13,102 @@ import LocationScreen from '../Screens/gps';
 import QuestCreateScreen from '../Screens/QuestCreateScreen';
 import QuestListScreen from './QuestListScreen';
 import { MapView } from 'expo';
-//dummy data to test quests
-//real one could have list like this filled in with function that finds all
-//quests within the map deltas
-const questData=[
-  {
-    key:1,
-    title: "Music",
-    description: "Lets listen to some music!",
-    latitude: 41.7398982169205,
-    longitude: -111.81636841058003,
-    type: 3,
-    pos: 45,
-    value: 4
-  },
-  {
-    key:2,
-    title: "Hike",
-    description: "Lets go on a hike!",
-    latitude: 41.7408982169205,
-    longitude: -111.81236841058003,
-    type: 1,
-    pos: 46,
-    value: 7
-  },
-  {
-    key:3,
-    title: "Dinner",
-    description: "Lets have some dinner!",
-    latitude: 41.7394982169205,
-    longitude: -111.81436841058003,
-    type: 2,
-    pos: 41,
-    value: 8
-  },
-  {
-    key:4,
-    title: "Education",
-    description: "Lets go learn something!",
-    latitude: 41.7418982169205,
-    longitude: -111.81436841058003,
-    type: 4,
-    pos: 41,
-    value: 8
-  }
-]
+
+const type = Object.freeze({"Physical":1, "Cultural":2, "Academic":3, "Other":4});
+const difficulty = Object.freeze(["Easy", "Medium", "Hard", "Extreme"]);
+
+questData = []
 
 function typeOfImage(props){
 
-  long=-111.79436841058003
-  lat=41.7535782169205
-  long=props.longitude
-  lat=props.latitude
-  tester={latitude:lat,longitude:long}
-  currpos={lat,long}
-  if (props.type==2){
-  return <MapView.Marker
-          key={props.key}
-          coordinate={tester}
-          title={props.title}
-          description={props.description}
-          image={require('../assets/dinner.png')}
-          //TODO When ready, this can be used to navigate to the quest's page
-          onCalloutPress={
-                  () => alert("yaaayyy")
-              }
-          style={{height:4, width:4}}
-          >
-    </MapView.Marker>
-  }
-  else if (props.type==1){
-  return <MapView.Marker
-          key={props.key}
-          coordinate={tester}
-          title={props.title}
-          description={props.description}
-          image={require('../assets/boots.png')}
+  long = -111.79436841058003
+  lat = 41.7535782169205
+  long = props.longitude
+  lat = props.latitude
+  tester = {latitude:lat,longitude:long}
+  currpos = {lat,long}
 
-          style={{height:4, width:4}}
-          >
-    </MapView.Marker>
+  if (props.type == 1){ // physical
+    return <MapView.Marker
+    key = {props.key}
+    coordinate = {tester}
+    title = {props.title}
+    description = {props.description}
+    image = {require('../assets/boots.png')}
+    //TODO When ready, this can be used to navigate to the quest's page
+    onCalloutPress = { () => Alert.alert(props.title,props.description) }
+    style = {{height:4, width:4}}
+      >
+      </MapView.Marker>
   }
-  else if (props.type==3){
-  return <MapView.Marker
-          key={props.key}
-          coordinate={tester}
-          title={props.title}
-          description={props.description}
-          image={require('../assets/music-player.png')}
-
-          style={{height:4, width:4}}
-          >
-    </MapView.Marker>
+  else if (props.type == 2){ // cultural
+    return <MapView.Marker
+    key = {props.key}
+    coordinate = {tester}
+    title = {props.title}
+    description = {props.description}
+    image = {require('../assets/music-player.png')}
+    onCalloutPress = { () => Alert.alert(props.title,props.description) }
+    style = {{height:4, width:4}}
+      >
+      </MapView.Marker>
   }
-  else if (props.type==4){
-  return <MapView.Marker
-          key={props.key}
-          coordinate={tester}
-          title={props.title}
-          description={props.description}
-          image={require('../assets/bookshelf.png')}
-
-          style={{height:4, width:4}}
-          >
-    </MapView.Marker>
+  else if (props.type == 3){ // academic
+    return <MapView.Marker
+    key = {props.key}
+    coordinate = {tester}
+    title = {props.title}
+    description = {props.description}
+    image = {require('../assets/bookshelf.png')}
+    onCalloutPress = { () => Alert.alert(props.title,props.description) }
+    style = {{height:4, width:4}}
+      >
+      </MapView.Marker>
+  }
+  else if (props.type == 4){ // other
+    return <MapView.Marker
+    key = {props.key}
+    coordinate = {tester}
+    title = {props.title}
+    description = {props.description}
+    image = {require('../assets/dinner.png')}
+    onCalloutPress = { () => Alert.alert(props.title,props.description) }
+    style = {{height:4, width:4}}
+      >
+      </MapView.Marker>
   }
 }
 
 class HomeScreen extends Component {
+
+  loaded = false;
+
+  constructor(props){
+    super(props);
+    AsyncStorage.getItem('localQuestList').then( value => {
+      this.setState({'localQuestList': JSON.parse(value) });
+      console.log('mounting and set: ' + JSON.stringify(this.state.localQuestList));
+      let list = this.state.localQuestList.list;
+      console.log("building list");
+      for (let i = 0; i < list.length; ++i) {
+        questData.push({
+          key:i,
+          title: list[i].name,
+          description: list[i].description + "\nType:          " + list[i].type +
+          "\nDifficulty:   " + difficulty[list[i].difficulty],
+          latitude: list[i].latitude,
+          longitude: list[i].longitude,
+          type: type[list[i].type],
+          pos: 45, // What is this for??
+          value: 4 // And this???
+        });
+      }
+      console.log(JSON.stringify(questData),'building finished');
+      this.loaded = true;
+      this.forceUpdate();
+    });
+    console.log(JSON.stringify(questData),'constructor finished');
+  }
 
   componentDidMount() {
     this.watchId = navigator.geolocation.watchPosition(
@@ -142,45 +130,47 @@ class HomeScreen extends Component {
 
   state = {
 
-  //The this marks Accra
-  mapRegion: { latitude: 5.8037, longitude: 0.2870, latitudeDelta: 0.0922, longitudeDelta: 0.0421 },
-};
-  static navigationOptions = {
-    drawerIcon: (
-      <Image source={require('../assets/homeIcon.png')}
-             style={{height:24, width:24}} />
-    )
-  }
+    //The this marks Accra
+    mapRegion: {
+      latitude: 5.8037,
+        longitude: 0.2870,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+    },
+  };
+
+
+
   render() {
 
 
     return (
       <Container>
-        <Header style={{backgroundColor:'#246A73'}}>
-          <Left>
-            <Button transparent>
-              <Icon name='menu' onPress={()=>this.props.navigation.navigate('DrawerOpen')} />
-            </Button>
-          </Left>
-        </Header>
+      <Header style={{backgroundColor:'#246A73'}}>
+      <Left>
+      <Button transparent>
+      <Icon name='menu' onPress={()=>this.props.navigation.navigate('DrawerOpen')} />
+      </Button>
+      </Left>
+      </Header>
 
 
-        <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: this.state.latitude,
+      <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: this.state.latitude,
           longitude: this.state.longitude,
           latitudeDelta: 0.1844,
           longitudeDelta: 0.0842,
-        }}
-        showsUserLocation={true}
-        followsUserLocation={true}
-        onPanDrag={e=>console.log(e.nativeEvent)}
+      }}
+      showsUserLocation={true}
+      followsUserLocation={true}
+      onPanDrag={e=>console.log(e.nativeEvent)}
       >
       {questData.map(function(name){
-                    const loc=name.value
-                    return <View>{typeOfImage(name)}</View>;
-                  })}
+        const loc=name.value
+        return <View>{typeOfImage(name)}</View>;
+      })}
 
 
       </MapView>
@@ -189,21 +179,28 @@ class HomeScreen extends Component {
       </Container>
     );
   }const
+
+  static navigationOptions = {
+    drawerIcon: (
+      <Image source={require('../assets/homeIcon.png')}
+      style={{height:24, width:24}} />
+    )
+  }
 }
 
 const CustomDrawerContent = (props) => (
   <Container>
-    <Header style = {{ height:200, backgroundColor:'#3685B5'}}>
-      <Body>
-        <Image
-          style={styles.drawerImage}
-          source={require('../assets/QuestMarker.jpg')}
-        />
-      </Body>
-    </Header>
-    <Content>
-      <DrawerItems {...props}/>
-    </Content>
+  <Header style = {{ height:200, backgroundColor:'#3685B5'}}>
+  <Body>
+  <Image
+  style={styles.drawerImage}
+  source={require('../assets/QuestMarker.jpg')}
+  />
+  </Body>
+  </Header>
+  <Content>
+  <DrawerItems {...props}/>
+  </Content>
   </Container>
 )
 
@@ -236,8 +233,8 @@ const HomeScreenTabNav = TabNavigator (
 styles = StyleSheet.create({
   drawerImage:{
     height:150,
-    width:150,
-    borderRadius: 75
+      width:150,
+      borderRadius: 75
   }
 });
 
--- Screens/QuestListScreen.js
+++ Screens/QuestListScreen.js
@@ -13,12 +13,12 @@ class QuestListScreen extends Component {
 
   constructor(props){
     super(props);
-		AsyncStorage.getItem('localQuestList').then( value => {
-			this.setState({'localQuestList': JSON.parse(value) });
-			console.log('mountin and set: ' + this.state.localQuestList);
+    AsyncStorage.getItem('localQuestList').then( value => {
+      this.setState({'localQuestList': JSON.parse(value) });
+      console.log('mountin and set: ' + this.state.localQuestList);
       this.ree = true;
       this.forceUpdate();
-		});
+    });
     console.log('construct');
   }
 
@@ -26,9 +26,9 @@ class QuestListScreen extends Component {
     this.setState({'localQuestList': {'list': []}});
   }
 
-	saveQuestList() {
-		AsyncStorage.setItem('localQuestList',JSON.stringify(this.state.localQuestList));
-	}
+  saveQuestList() {
+    AsyncStorage.setItem('localQuestList',JSON.stringify(this.state.localQuestList));
+  }
   deleteQuest(idx) {
     this.state.localQuestList.list.splice(idx,1);
     this.saveQuestList();
@@ -55,56 +55,56 @@ class QuestListScreen extends Component {
 
   getQuests() {
     let quests = this.state.localQuestList.list.map(( item, idx ) =>
-    {
-      return(
+      {
+        return(
           <View key={idx} style = {{height:200,width:'90%',backgroundColor:'#26A69A',alignItems:'center',alignSelf:'center',margin: 4}}>
-            <Text style= {{'fontWeight': 'bold'}}> {item.name}</Text>
-            <ScrollView style={{backgroundColor:'blue', width:'90%', position:'absolute','top':'10%',bottom:'20%'}}>
-                <Text style= {{flex:1, alignSelf:'center', fontWeight:'bold'}}>Description</Text>
-                <Text style= {{flex:1, alignSelf:'center'}}> {item.description}</Text>
-                <Text style= {{flex:1, alignSelf:'center', fontWeight:'bold'}}>Type</Text>
-                <Text style= {{flex:1, alignSelf:'center'}}> {item.type}</Text>
-                <Text style= {{flex:1, alignSelf:'center', fontWeight:'bold'}}>Difficulty</Text>
-                <Text style= {{flex:1, alignSelf:'center'}}> {this.getDifficulty(item)}</Text>
-                <Text style= {{flex:1, alignSelf:'center', fontWeight:'bold'}}>Location</Text>
+          <Text style= {{'fontWeight': 'bold'}}> {item.name}</Text>
+          <ScrollView style={{backgroundColor:'blue', width:'90%', position:'absolute','top':'10%',bottom:'20%'}}>
+          <Text style= {{flex:1, alignSelf:'center', fontWeight:'bold'}}>Description</Text>
+          <Text style= {{flex:1, alignSelf:'center'}}> {item.description}</Text>
+          <Text style= {{flex:1, alignSelf:'center', fontWeight:'bold'}}>Type</Text>
+          <Text style= {{flex:1, alignSelf:'center'}}> {item.type}</Text>
+          <Text style= {{flex:1, alignSelf:'center', fontWeight:'bold'}}>Difficulty</Text>
+          <Text style= {{flex:1, alignSelf:'center'}}> {this.getDifficulty(item)}</Text>
+          <Text style= {{flex:1, alignSelf:'center', fontWeight:'bold'}}>Location</Text>
 
-            </ScrollView>
-            <View style={{
-              height:20,
+          </ScrollView>
+          <View style={{
+            height:20,
               position:'absolute',
               bottom:10,
-              flex:1, 
-              flexDirection: 'row', 
-              width:'80%', 
-            }}>
-            <View style={{flex:1}}>
-              <Button block style = {{
-                height:20,
-                position:'absolute',
-                bottom:0,
-                backgroundColor:'#ffd150',
-                width:'90%', 
-                alignSelf:'center'}} 
-                onPress={()=> this.editQuest(idx)}>
-                <Text> Edit </Text>
-              </Button>
-            </View>
-            <View style={{flex:1}}>
-              <Button block style = {{
-                height:20,
-                position:'absolute',
-                bottom:0,
-                backgroundColor:'#cc5050',
-                width:'90%', 
-                alignSelf:'center',
-                }} onPress={()=> this.deleteQuest(idx)}>
-                <Text> Delete </Text>
-              </Button>
-            </View>
-            </View>
+              flex:1,
+              flexDirection: 'row',
+              width:'80%',
+          }}>
+          <View style={{flex:1}}>
+          <Button block style = {{
+            height:20,
+              position:'absolute',
+              bottom:0,
+              backgroundColor:'#ffd150',
+              width:'90%',
+              alignSelf:'center'}}
+          onPress={()=> this.editQuest(idx)}>
+          <Text> Edit </Text>
+          </Button>
           </View>
-      );
-    });
+          <View style={{flex:1}}>
+          <Button block style = {{
+            height:20,
+              position:'absolute',
+              bottom:0,
+              backgroundColor:'#cc5050',
+              width:'90%',
+              alignSelf:'center',
+          }} onPress={()=> this.deleteQuest(idx)}>
+          <Text> Delete </Text>
+          </Button>
+          </View>
+          </View>
+          </View>
+        );
+      });
     return quests;
   }
 
@@ -114,27 +114,27 @@ class QuestListScreen extends Component {
     }
     console.log(this.state.localQuestList);
     return (
-    <Container>
+      <Container>
       <Header style={{backgroundColor:'#246A73'}}>
-        <Left>
-          <Button transparent>
-            <Icon name='menu' onPress={()=>this.props.navigation.navigate('DrawerOpen')} />
-          </Button>
-        </Left>
+      <Left>
+      <Button transparent>
+      <Icon name='menu' onPress={()=>this.props.navigation.navigate('DrawerOpen')} />
+      </Button>
+      </Left>
       </Header>
       <Content contentContainerStyle= {{
-          flex: 1,
-            alignItems: 'center',
-            justifyContent : 'center',
-            width: '100%',
-        }}>
-          <Text style = {{'fontWeight':'bold', 'fontSize':20}}>Quest Log</Text>
-          <ScrollView style= {{width:'90%'}}>
-          {
-            this.getQuests()
-          }
-          </ScrollView>
-        </Content>
+        flex: 1,
+          alignItems: 'center',
+          justifyContent : 'center',
+          width: '100%',
+      }}>
+      <Text style = {{'fontWeight':'bold', 'fontSize':20}}>Quest Log</Text>
+      <ScrollView style= {{width:'90%'}}>
+      {
+        this.getQuests()
+      }
+      </ScrollView>
+      </Content>
       </Container>
     );
   }
